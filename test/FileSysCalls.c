e739178b43d62257d1269f64d03fda7996594ca7
Whitney Lai <whitnlai@gmail.com>
2013-03-21 11:01:41 -0700

    some test case reworking

--- test/FileSysCalls.c
+++ test/FileSysCalls.c
@@ -2,59 +2,88 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-#define size 1024
-#define bigsize 2560
+#define BUFFSIZE 1024
+#define XLPAGESIZE 2560
 
-char buf1[size], buf2[size], buf3[bigsize], buf4[bigsize];
+char buf1[PAGESIZE], buf2[PAGESIZE], buf3[XLPAGESIZE], buf4[XLPAGESIZE];
 
 int main(void){
-	//Tests for any modifications to the FD Array (Create, Unlink & Close)
-	int i, daFile, writeDaFile, readDaFile, openDaFile, closeDaFile, unlinkDaFile;
+	int i = 0;
+	int file;
+
+	//int writeFile, readFile, openFile;
 	char fileName[1000];
 
+	/* [TEST SET ONE]
+	 * tests for the following syscalls: creat, close, unlink
+	 */
+	int closeFile, unlinkFile;
+	for(i =0; i<20; i++){
+		// testing creat syscall; will attempt to create the test file 20 times
+		file = creat("testFile.txt");
+		if(file == -1){
+			printf("[ERROR] creat failed for testFile.txt on %d-th attempt", i);
+			return 1;
+		}
 
-	for(i =0; i<17; i++){
-		daFile = creat("test1.txt");
-		if(daFile == -1){
-			printf("ERROR: Create failed on FD %d", i);
+		// testing close syscall; will attempt to close the test file 20 times
+		closeFile = close(file);
+		if(closeFile == -1){
+			printf("[ERROR] close failed for testFile.txt on %d-th attempt", i);
 			return 1;
 		}
-		closeDaFile = close(daFile);
-		if(closeDaFile == -1){
-			printf("ERROR: Close failed on FD %d", i);
-			return 1;
+
+		// testing unlink syscall; will attempt to delete the test file 20 times
+		unlinkFile = unlink(file);
+		if(unlinkFile == -1) {
+			printf("[ERROR] unlink failed for testFile.txt on %d-th attempt", i);
 		}
-		unlink("test.txt");
 	}
 
-	//Tests for Read & Write
-	daFile = creat("test2.txt");
-	if(daFile == -1){
-		printf("ERROR: Create failed on test2.txt");
+	/* [TEST SET ONE]
+	 * tests for the following syscalls: read, write
+	 */
+	char writeBuffer[BUFFSIZE];
+	char readBuffer[BUFFSIZE];
+	int writeFile, readFile;
+	file = creat("testFile2.txt");
+	if(file == -1){
+		printf("[ERROR] Create failed on test2.txt");
 		return 1;
 	}
+
+	// testing write syscall; will attempt to write 64 bytes to the newly created test file then close it
 	for(i=0; i<64; i++){
-		buf1[i] = 'x' + i;
+		writeBuffer[i] = ".";
 	}
-	writeDaFile = write(daFile, buf1, 64);
-	if(writeDaFile == -1){
-		printf("ERROR: Couldn't write to test2.txt");
+	writeFile = write(file, writeBuffer, 64);
+	if(writeFile == -1){
+		printf("[ERROR] write failed for testFile2.txt");
 		return 1;
 	}
-	close(daFile);
-	daFile = open("test2.txt");
-	readDaFile = read(daFile, buf2, 64);
-	if(readDaFile == -1){
-		printf("ERROR: Couldn't read test2.txt");
+	else {
+		printf("[INFO] write succeeded for testFile2.txt; wrote %d bytes", writeFile);
+	}
+	close(file);
+
+	// testing read syscall; will attempt to open the newly created and written test file then read it
+	file = open("testFile2.txt");
+	readFile = read(file, readBuffer, 64);
+	if(readFile == -1){
+		printf("[ERROR] read failed for testFile2.txt");
 		return 1;
 	}
+	else {
+		printf("[INFO] read succeeded for testFile2.txt; read %d bytes", readFile);
+	}
+
+	// double check that what we wrote = what we read
 	for(i=0; i<64; i++){
-		if (buf2[i] != buf1[i]){
-			printf("ERROR: Wrong contents in test2.txt");
+		if (readBuffer[i] != writeBuffer[i]){
+			printf("[ERROR] content written to testFile2.txt does not match content read from testFile2.txt; failed at %d-th byte", i);
 			return 1;
 		}
 	}
-
 
 	//Unlink while a source is accessing it (Pending for deletion)
 	unlinkDaFile = unlink("test2.txt");
