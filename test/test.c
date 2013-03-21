bd8c9d829d46425c30b36c454aaf1f65b580cd64
bhaviksingh <bhaviksingh@gmail.com>
2013-03-22 00:40:18 +0530

    added part 3 test cases, bug fixes

---
+++ test/test.c
@@ -0,0 +1,13 @@
+#include "syscall.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+
+int main( int argc, const char* argv[] )
+{
+	printf("TESTING called from joined program \n");
+        printf("Number of arguments: %d \n", argc);
+
+	return 0;
+
+}
